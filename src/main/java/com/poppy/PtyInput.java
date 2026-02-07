72c3f79275c9cc88f37697298d22618a810eba07
TheYautja <am.96138669@gmail.com>
2026-02-07 17:43:18 -0300

    PtyInput now uses a byte buffer

--- src/main/java/com/poppy/PtyInput.java
+++ src/main/java/com/poppy/PtyInput.java
@@ -13,9 +13,21 @@ class PtyInput implements Runnable {
 	}
 	
 	public void run(){
-
+		
+		byte[] buffer = new byte[4096];
+		
 		try {
-			System.out.println("This is the reader :O");
+			
+			while(!Thread.currentThread().isInterrupted()){
+				
+				int n = is.read(buffer);
+				if(n == -1)break;
+				
+				String res = new String(buffer, 0, n);
+				System.out.print(res);
+				
+			}
+	
 		}catch(IOException e){
 			System.out.println("\u001B[34m" + "Pty Input stream error :(" + "\u001B[0m");
 			e.printStackTrace();
