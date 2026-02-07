46262643ee460d093e6b502eb798cd47e6b3b180
TheYautja <am.96138669@gmail.com>
2026-02-06 22:28:24 -0300

    PTY now implements Runnable

--- src/main/java/com/poppy/PTY.java
+++ src/main/java/com/poppy/PTY.java
@@ -1,13 +1,24 @@
 
+package com.poppy;
 
-class PTY {
+import com.pty4j.PtyProcess;
+import com.pty4j.PtyProcessBuilder;
+
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.Scanner;
+
+
+class PTY implements Runnable {
 	
 	public PTY(){
-		initPTY();
+		run();
 	}
 	
-	public void initPTY(){
-		
+	public void run(){
+				
 		String[] cmd = { "/bin/sh", "-l" };
 		Map<String, String> env = new HashMap<>(System.getenv());
 		if (!env.containsKey("TERM")) env.put("TERM", "xterm");
@@ -15,11 +26,19 @@ class PTY {
 
 		OutputStream os = process.getOutputStream();
 		InputStream is = process.getInputStream();
+				
+		String teste = "ls\n";
+				
+		os.write(teste.getBytes());
+		os.flush();
+				
+		//byte[] buf = new byte[1024];
+		//int n = is.read(buf);
+		//System.out.print(new String(buf, 0, n));
+				
 
-		int result = process.waitFor();
-		
+		//int result = process.waitFor();
+				
 	}
-	
-
 	
 }
